f43c2bbfacd3e0e2ecae119d148acf19e75960c5
James Lee <jamestlee513@gmail.com>
2020-10-20 13:30:39 -0700

    Finished phase 5

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const routes = require("./routes");
 const port = 8081;
 
 app.set("view engine", "pug");
@@ -7,7 +8,6 @@ app.set("view engine", "pug");
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
-
 
 app.get(/xyz$/, (req, res) => {
   res.send("That's all I wrote.");
@@ -17,7 +17,11 @@ app.get("/capital-letters/:upper", (req, res) => {
   res.send(req.params.upper.toUpperCase());
 });
 
-app.all("*", (req, res) => {
+app.use("/margot", routes);
+
+app.use("/margeaux", routes);
+
+app.all(/^\/[^\/]+$/, (req, res) => {
   let randomNumber = Math.floor(Math.random() * 10);
   res.render("template", { method: req.method, path: req.path, randomNumber });
 });
