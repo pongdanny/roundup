bfdca13f6e802fe8224bd960720fcf0df1968b7c
danny pong <dannybankx@dannys-MacBook-Air.local>
2020-10-20 14:15:46 -0400

    phase 2 complete

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 const port = 8081;
 
-app.get("/", (req, res) => {
-  res.send("this works!");
+app.set("view engine", "pug");
+
+// app.get("/", (req, res) => {
+//   res.send("this works!");
+// });
+
+app.all("*", (req, res) => {
+  let randomNumber = Math.floor(Math.random() * 10);
+  res.render("template", { method: req.method, path: req.path, randomNumber });
 });
 
 app.listen(port, () => console.log(`Running on port ${port}`));
