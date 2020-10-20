10246f922f224d6ffea4d71f2cfed1891721eb7b
James Lee <jamestlee513@gmail.com>
2020-10-20 12:56:57 -0700

    Finished phase 4

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const port = 8081;
 
 app.set("view engine", "pug");
 
-// app.get("/", (req, res) => {
-//   res.send("this works!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+
+app.get(/xyz$/, (req, res) => {
+  res.send("That's all I wrote.");
+});
+
+app.get("/capital-letters/:upper", (req, res) => {
+  res.send(req.params.upper.toUpperCase());
+});
 
 app.all("*", (req, res) => {
   let randomNumber = Math.floor(Math.random() * 10);
